5e5055ec031ebf724a8c9857d54584bbc8c8c5bb
Ernest Korzeniowski <ernest.k@int.pl>
2025-10-18 09:01:35 +0200

    Removing comment and animation

--- src/Assignments/01_House/shaders/base_fs.glsl
+++ src/Assignments/01_House/shaders/base_fs.glsl
@@ -1,10 +1,10 @@
 #version 410
 
-layout(location=0) out vec4 vFragColor;
+layout(location=0)out vec4 vFragColor;
 
 uniform float iTime;
 
-void main() {
-
-    vFragColor = vec4(0.5+sin(iTime)*.5, 0.0, 0.0, 1.0);
+void main(){
+    
+    vFragColor=vec4(1,0.,0.,1.);
 }
